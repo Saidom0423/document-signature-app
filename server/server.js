d12c4b3ad2dfae32ddd16911cffe071263c5ae7b
Sai Dom <Saidom0423@gmail.com>
2026-02-28 18:13:04 +0530

    init

--- server/server.js
+++ server/server.js
@@ -5,16 +5,24 @@ require("dotenv").config();
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+// Routes
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
 
+// Test Route
 app.get("/", (req, res) => {
   res.send("API Running");
 });
 
+// Database Connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log("Mongo Error:", err));
+
+// Server Start
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
